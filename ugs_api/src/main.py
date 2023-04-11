db61f3eee43c770f6de4eceee6901bf56fc82427
Pavel Krizhepolskiy <re-times@yandex.ru>
2023-04-11 21:34:08 +0300

    Базовая модель с поддержкой ORJSON

--- ugs_api/src/main.py
+++ ugs_api/src/main.py
@@ -3,7 +3,7 @@
 Для удобства дебага можно добавить:
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=settings, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
 
 """
 from uuid import uuid4
