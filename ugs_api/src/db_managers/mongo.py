d9f10a35bcd8ed331d5ead9a60720f2f11d9aa37
Pavel Krizhepolskiy <re-times@yandex.ru>
2023-04-15 04:58:36 +0300

    Добавил метод aggregate для информации из разных таблиц

--- ugs_api/src/db_managers/mongo.py
+++ ugs_api/src/db_managers/mongo.py
@@ -72,6 +72,55 @@ class MongoManager(AbstractDBManager):
         collection = self._open_collection(table)
         await collection.delete_many(search)
 
+    def aggregate(
+        self,
+            table: str,
+            search: dict,
+            related_table: str,
+            related_fields: list,
+            desired_property: str,
+            limit: int,
+            offset: int,
+            sort: list,
+    ):
+        """Создание запроса для агрегирования данных.
+
+        Объединяет две таблицы (коллекции) на основе двух связанных полей.
+        Создает поля со средним и суммарным значением искомого атрибута.
+        Добавляет поля "avg" и "sum" которые можно использовать для сортировки.
+        Пример: добавляет рейтинг и среднюю оценку и обзору.
+
+        Args:
+          table: название таблицы (коллекции) БД;
+          search: словарь с данными для поиска;
+          related_table: название связанной таблицы (коллекции) БД;
+          related_fields: названия связанных полей локального и внешнего объекта;
+          desired_property: искомый атрибут у связанного объекта;
+          limit: кол-во элементов в выдаче;
+          offset: смещение (пропуск первых N элементов);
+          sort: список полей и направление сортировки [('avg', 1)].
+
+        """
+        collection = self._open_collection(table)
+        lookup = {
+            'from': related_table,
+            'localField': related_fields[0],
+            'foreignField': related_fields[1],
+            'as': 'related_object',
+        }
+        avg_filed = {'$avg': '$related_object.{0}'.format(desired_property)}
+        sum_field = {'$sum': '$related_object.{0}'.format(desired_property)}
+        return collection.aggregate(
+            [
+                {'$match': search},
+                {'$lookup': lookup},
+                {'$addFields': {'avg': avg_filed, 'sum': sum_field}},
+                {'$sort': {sort[0]: sort[1]}},
+                {'$skip': offset},
+                {'$limit': limit},
+            ],
+        ).to_list(length=limit)
+
     def _open_collection(self, collection_name: str):
         """Переходим к нужной коллекции."""
         return self.db[collection_name]
@@ -81,5 +130,5 @@ class MongoManager(AbstractDBManager):
 def get_db_manager(
     client: AsyncIOMotorClient = Depends(get_mongo),
 ) -> AbstractDBManager:
-    """DI для FastAPI."""
+    """DI для FastAPI. Получаем менеджер для MONGO."""
     return MongoManager(client=client, db_name=settings.MONGO_DB_NAME)
